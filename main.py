b26d1a81e6d0d6427f72df61c5902f41000ee06c
xohyver <154579145+xohyver@users.noreply.github.com>
2025-03-07 17:23:32 +0900

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ model = torch.hub.load('ultralytics/yolov5', 'custom', path="./Subway_Crowd_cont
 
 #사전 설정
 pygame.mixer.init()
-is_audio_playing = False
+is_audio_playing = False  #음성 중복 재생을 막기위한 변수 설정
 
 cap = cv2.VideoCapture(0)
 
@@ -27,13 +27,13 @@ while True:
     if not ret:
         break
 
-    frame_rgb = frame[:, :, ::-1]
+    frame_rgb = frame[:, :, ::-1]  #rgb를 bgr로 변환
     results = model(frame_rgb)
     
-    head_count = sum(1 for *box, label, conf in results.xyxy[0] if int(label) == 0)
+    head_count = sum(1 for *box, label, conf in results.xyxy[0] if int(label) == 0)  #모델을 통해 카운트 된 사람 머리 개수
 
     if not is_audio_playing and head_count > max_people:
-        Thread(target=play_audio).start()
+        Thread(target=play_audio).start() #Thread 활용하여 병렬처리
         is_audio_playing = True
         
     elif is_audio_playing and not pygame.mixer.music.get_busy():
